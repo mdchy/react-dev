ddca92322b0b9d60d3eebfd12c08353c442e252d
mdchy <mdchy5000@gmail.com>
2024-11-26 22:36:51 +0600

    office commit

--- air-bnb-clone/src/components/Meme.js
+++ air-bnb-clone/src/components/Meme.js
@@ -3,11 +3,12 @@ import './meme.css'
 import memeData from "./data/memeData"
 
 export default function Meme(){
+    const [imgUrl, setImgUrl] = React.useState("https://i.imgflip.com/1g8my4.jpg")
     function getRandomMeme(){
         const memeArray = memeData.data.memes
         const randomNum = Math.floor(Math.random() * memeArray.length)
         const url = memeArray[randomNum].url
-        console.log(url)
+        setImgUrl(url)
     }
     return(
         <main>
@@ -15,6 +16,9 @@ export default function Meme(){
                 <input type='text' className='imput--field' placeholder='Top text'/>
                 <input type='text' className='input--field' placeholder='Bottom text'/>
                 <button className='form--button' onClick={getRandomMeme}>Get a new image</button>
+            </div>
+            <div className='meme-container'>
+                <img src={imgUrl} alt='meme'></img>
             </div>
         </main>
     )
